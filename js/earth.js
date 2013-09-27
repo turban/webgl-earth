6d7afce38ad7fe1ee51134a8cde611667c34a4d1
Bjørn Sandvik <bjorn@thematicmapping.org>
2013-09-27 16:28:33 +0200

    New version

--- js/earth.js
+++ js/earth.js
@@ -1,163 +1,125 @@
-	var renderer	= new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
+$(function() {
 
-	var onRenderFcts= [];
+    //var stats = initStats();
+
+	// Scene size
+	var width  = window.innerWidth,
+		height = window.innerHeight;
+
+	// Sphere
+	var radius    = 0.5,
+		segments  = 32;
+
+	// Camera position
+	var angle  = 45,
+		aspect = width / height,
+		near   = 0.01,
+		far    = 1000,
+		zoom   = 1.5;
+
+	var camera = new THREE.PerspectiveCamera(angle, aspect, near, far);
+	camera.position.z = zoom;
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+
+	$("#webgl").append(renderer.domElement);
+
 	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.z = 1.5;
 
-	/*
-	var light = new THREE.AmbientLight(0x888888);
-	scene.add(light);
-	*/
+	var light = new THREE.AmbientLight(0x333333)
+	scene.add(light)
 
 	var light = new THREE.DirectionalLight(0xffffff, 1);
 	light.position.set(5,3,5);
 	scene.add(light);
 
-	//////////////////////////////////////////////////////////////////////////////////
-	//		add an object and make it move					//
-	//////////////////////////////////////////////////////////////////////////////////	
+    // the first argument of THREE.SphereGeometry is the radius, the second argument is
+    // the segmentsWidth, and the third argument is the segmentsHeight.  Increasing the 
+    // segmentsWidth and segmentsHeight will yield a more perfect circle, but will degrade
+    // rendering performance
+    // http://www.html5canvastutorials.com/three/html5-canvas-webgl-sphere-with-three-js/
 
-	var geometry	= new THREE.SphereGeometry(0.5, 32, 32);
+    var sphere = createSphere(radius, segments, segments);
+	scene.add(sphere)
 
-	var material	= new THREE.MeshPhongMaterial({
-		map			: THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
-		bumpMap		: THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
-		bumpScale	: 0.05,
-		specularMap	: THREE.ImageUtils.loadTexture('images/water_4k.png'),
-		specular	: new THREE.Color('grey')
-	});
+    var clouds = createClouds(radius, segments, segments);
+	scene.add(clouds)
 
-	var earthMesh	= new THREE.Mesh(geometry, material);
-
-	scene.add(earthMesh)
-	onRenderFcts.push(function(delta, now){
-		earthMesh.rotateY( 1/32 * delta )
-	})
+	var stars = createStars();
+	scene.add(stars);
 
 
+    var step = 0;
+
+	// Setup Control GUI
+	var controls = new function () {
+		this.radius = radius;
+        this.segments = segments;
+
+		this.redraw = function () {
+			scene.remove(sphere);
+			sphere = createSphere(controls.radius, controls.segments, controls.segments);
+			scene.add(sphere);
+        };
+    }
+
+    /*
+	var gui = new dat.GUI();
+	gui.add(controls, 'radius', 1, 400).onChange(controls.redraw);
+	gui.add(controls, 'segments', 1, 100).onChange(controls.redraw);
+	*/
+
+	render();
+
+	function render() {
+		//stats.update();
+		sphere.rotation.y = step += 0.001;
+		clouds.rotation.y = step += 0.001;		
+		requestAnimationFrame(render);
+		renderer.render(scene, camera);
+	}
+
+	function initStats() {
+		var stats = new Stats();
+		stats.setMode(0); // 0: fps, 1: ms
+		$("#stats").append(stats.domElement);
+		return stats;
+	}
+
+	function createSphere(radius, widthSegments, heightSegments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, widthSegments, heightSegments),
+			new THREE.MeshPhongMaterial({
+				map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
+				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
+				bumpScale:   0.002,
+				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
+				specular:    new THREE.Color('grey')								
+			})
+		);
+	}
+
+	function createClouds(radius, widthSegments, heightSegments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius + 0.003, widthSegments, heightSegments),
+			new THREE.MeshPhongMaterial({
+				map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.neuquant.png'),
+				transparent: true
+			})
+		);		
+	}
+
+	function createStars() {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(90, 32, 32), 
+			new THREE.MeshBasicMaterial({
+				map: THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'),
+				side: THREE.BackSide
+			})
+		);
+	}
 
 
+});
 
-	// CLOUDS
-
-	var canvasResult	= document.createElement('canvas');
-	canvasResult.width	= 4096;
-	canvasResult.height	= 2048;
-	var contextResult	= canvasResult.getContext('2d')		
-
-	// load earthcloudmap
-	var imageMap	= new Image();
-
-	imageMap.addEventListener("load", function() {
-		
-		// create dataMap ImageData for earthcloudmap
-		var canvasMap	= document.createElement('canvas')
-		canvasMap.width	= imageMap.width
-		canvasMap.height= imageMap.height
-		var contextMap	= canvasMap.getContext('2d')
-		contextMap.drawImage(imageMap, 0, 0)
-		var dataMap	= contextMap.getImageData(0, 0, canvasMap.width, canvasMap.height)
-
-		// load earthcloudmaptrans
-		var imageTrans	= new Image();
-		imageTrans.addEventListener("load", function(){
-			// create dataTrans ImageData for earthcloudmaptrans
-			var canvasTrans		= document.createElement('canvas')
-			canvasTrans.width	= imageTrans.width
-			canvasTrans.height	= imageTrans.height
-			var contextTrans	= canvasTrans.getContext('2d')
-			contextTrans.drawImage(imageTrans, 0, 0)
-			var dataTrans		= contextTrans.getImageData(0, 0, canvasTrans.width, canvasTrans.height)
-			// merge dataMap + dataTrans into dataResult
-			var dataResult		= contextMap.createImageData(canvasMap.width, canvasMap.height)
-			for(var y = 0, offset = 0; y < imageMap.height; y++){
-				for(var x = 0; x < imageMap.width; x++, offset += 4){
-					dataResult.data[offset+0] = dataMap.data[offset+0]
-					dataResult.data[offset+1] = dataMap.data[offset+1]
-					dataResult.data[offset+2] = dataMap.data[offset+2]
-					dataResult.data[offset+3] = 255 - dataTrans.data[offset+0]
-				}
-			}
-			// update texture with result
-			contextResult.putImageData(dataResult,0,0)	
-			material.map.needsUpdate = true;
-		})
-		imageTrans.src	= 'images/fair_clouds_4k.jpg';
-	}, false);
-
-	imageMap.src	= 'images/fair_clouds_4k.jpg';
-
-	var geometry	= new THREE.SphereGeometry(0.51, 32, 32)
-	var material	= new THREE.MeshPhongMaterial({
-		map		: new THREE.Texture(canvasResult),
-		side		: THREE.DoubleSide,
-		transparent	: true,
-		opacity		: 0.8,
-	})
-	var cloudMesh = new THREE.Mesh(geometry, material);
-
-	/*
-
-	scene.add(cloudMesh)
-	onRenderFcts.push(function(delta, now){
-		cloudMesh.rotateY( 1/32 * delta )
-	})
-	
-	*/	
-
-
-
-	//////////////////////////////////////////////////////////////////////////////////
-	//		add star field							//
-	//////////////////////////////////////////////////////////////////////////////////
-	
-	var geometry  = new THREE.SphereGeometry(90, 32, 32)
-	var material  = new THREE.MeshBasicMaterial()
-	material.map   = THREE.ImageUtils.loadTexture('images/starfield_4k.jpg')
-	material.side  = THREE.BackSide
-	var mesh  = new THREE.Mesh(geometry, material)
-	scene.add(mesh)
-
-
-
-	//////////////////////////////////////////////////////////////////////////////////
-	//		Camera Controls							//
-	//////////////////////////////////////////////////////////////////////////////////
-	var mouse	= {x : 0, y : 0}
-	document.addEventListener('mousemove', function(event){
-		mouse.x	= (event.clientX / window.innerWidth ) - 0.5
-		mouse.y	= (event.clientY / window.innerHeight) - 0.5
-	}, false)
-	onRenderFcts.push(function(delta, now){
-		camera.position.x += (mouse.x*5 - camera.position.x) * (delta*3)
-		camera.position.y += (mouse.y*5 - camera.position.y) * (delta*3)
-		camera.lookAt( scene.position )
-	})
-
-
-	//////////////////////////////////////////////////////////////////////////////////
-	//		render the scene						//
-	//////////////////////////////////////////////////////////////////////////////////
-	onRenderFcts.push(function(){
-		renderer.render( scene, camera );		
-	})
-	
-	//////////////////////////////////////////////////////////////////////////////////
-	//		loop runner							//
-	//////////////////////////////////////////////////////////////////////////////////
-	var lastTimeMsec= null
-	requestAnimationFrame(function animate(nowMsec){
-		// keep looping
-		requestAnimationFrame( animate );
-		// measure time
-		lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
-		var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
-		lastTimeMsec	= nowMsec
-		// call each update function
-		onRenderFcts.forEach(function(onRenderFct){
-			onRenderFct(deltaMsec/1000, nowMsec/1000)
-		})
-	})
