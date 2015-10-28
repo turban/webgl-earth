8cb76bb0eebae5e78c6aded26a3f4d9e40827f3e
Joan Climent <joandelason@gmail.com>
2015-10-28 22:02:20 +0100

    Removed unnecessary feautures

--- js/earth.js
+++ js/earth.js
@@ -28,15 +28,12 @@
 
 	var light = new THREE.DirectionalLight(0xffffff, 1);
 	light.position.set(5,3,5);
-	scene.add(light);
+	//scene.add(light);
 
     var sphere = createSphere(radius, segments);
 	sphere.rotation.y = rotation; 
 	scene.add(sphere)
 
-    var clouds = createClouds(radius, segments);
-	clouds.rotation.y = rotation;
-	scene.add(clouds)
 
 	var stars = createStars(90, 64);
 	scene.add(stars);
@@ -50,7 +47,7 @@
 	function render() {
 		controls.update();
 		sphere.rotation.y += 0.0005;
-		clouds.rotation.y += 0.0005;		
+
 		requestAnimationFrame(render);
 		renderer.render(scene, camera);
 	}
@@ -58,24 +55,10 @@
 	function createSphere(radius, segments) {
 		return new THREE.Mesh(
 			new THREE.SphereGeometry(radius, segments, segments),
-			new THREE.MeshPhongMaterial({
-				map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
-				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
-				bumpScale:   0.005,
-				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
-				specular:    new THREE.Color('grey')								
+			new THREE.MeshBasicMaterial({
+				map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg')
 			})
 		);
-	}
-
-	function createClouds(radius, segments) {
-		return new THREE.Mesh(
-			new THREE.SphereGeometry(radius + 0.003, segments, segments),			
-			new THREE.MeshPhongMaterial({
-				map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
-				transparent: true
-			})
-		);		
 	}
 
 	function createStars(radius, segments) {
